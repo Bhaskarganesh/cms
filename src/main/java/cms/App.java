3a95a6ae36650efbabe7e6e723549e5d555dd222
Bhaskarganesh <32055896+Bhaskarganesh@users.noreply.github.com>
2019-11-27 18:03:27 +0530

    Update App.java

--- src/main/java/cms/App.java
+++ src/main/java/cms/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World jenkinss" );
+        System.out.println( "Hello World jenkinsssss" );
     }
 }
